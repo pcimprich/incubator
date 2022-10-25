b3e63908411420dbaf0c5cb521c037ad038f3640
Petr Cimprich <pcimprich@gmail.com>
2022-10-25 22:00:33 +0200

    Create playground.js

---
+++ playground.js
@@ -0,0 +1,12 @@
+const animals = ['lion', 'tiger', 'zebra', 'giraffe', 'elephant', 'kangaroo', 'bear', 'fox'];
+
+const animals2 = [
+  { name: 'lion', continent: ['Africa', 'Asia'] },
+  { name: 'tiger', continent: ['Asia'] },
+  { name: 'zebra', continent: ['Africa'] },
+  { name: 'giraffe', continent: ['Africa'] },
+  { name: 'elephant', continent: ['Africa', 'Asia'] },
+  { name: 'kangaroo', continent: ['Australia'] },
+  { name: 'bear', continent: ['America', 'Europe', 'Asia'] },
+  { name: 'fox', continent: ['America', 'Europe', 'Asia', 'Africa'] }
+];
